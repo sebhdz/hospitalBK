922f6cdb5c387a4d04cb1f4db4f512e7c94354fe
GHzr <bolileo-25@hotmail.com>
2025-12-08 01:32:59 -0600

    fix: solucionado Error 500 en crear citas y mejoras en controladores

--- src/routes/pagos_routes.ts
+++ src/routes/pagos_routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { obtenerIngresos, crearPago } from '../controllers/pagos_controller';
+import { obtenerIngresos, crearPago, obtenerTotalHoy } from '../controllers/pagos_controller';
 
 const router = Router();
 
 router.get('/ingresos', obtenerIngresos);
 router.post('/', crearPago);
+router.get('/hoy', obtenerTotalHoy);
 
 export default router;
